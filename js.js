d6cc4a3f6d7c4e2101c2b0f2bcdfa83d4a4db87d
j.kunciene@gmail.com <48964713+jkunciene@users.noreply.github.com>
2020-04-28 14:02:06 +0300

    antras uzdavinys su switch, pazymiai

--- js.js
+++ js.js
@@ -5,3 +5,48 @@ if(n%m ==0){
     k=n/m;
 } else {k=Math.ceil(n/m)}
 console.log(k);
+
+
+const button= document.querySelector('button')
+const reiksme = document.querySelector('input')
+const rezultatas= document.getElementById('irasui')
+
+button.addEventListener('click', setPazymys)
+function setPazymys() {
+    let pazymys = Number(reiksme.value);
+    console.log(typeof pazymys, pazymys);
+    switch (pazymys) {
+
+        case 1:
+            text = "Ojei kaip baisu"
+            break;
+        case 2:
+            text = "Ojei kaip baisu"
+            break;
+        case 3:
+            text = "Ojei kaip baisu"
+            break;
+        case 4:
+            text = "Ojei kaip baisu"
+            break;
+        case 5:
+            text = "Teigiamas, bet tu gali geriau"
+        case 6:
+            text = "Teigiamas, bet tu gali geriau"
+        case 7:
+            text = "Teigiamas, bet tu gali geriau"
+        case 8:
+            text = "Geras"
+            break;
+        case 9:
+            text = "Labai gerai"
+            break;
+        case 10:
+            text = "Puiku relax"
+            break;
+        default:
+            text = "Įveskite pažymio reikšmę sveikaisiais skaičiais"
+            break;
+    }
+    rezultatas.innerHTML = text;
+}
